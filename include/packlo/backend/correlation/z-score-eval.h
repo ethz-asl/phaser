d08e6d2b6fcc479681f53dd42dc1ded4e58cc7fa
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2019-10-19 20:33:22 +0200

    some refactoring on the z-score calculation

--- include/packlo/backend/correlation/z-score-eval.h
+++ include/packlo/backend/correlation/z-score-eval.h
@@ -7,14 +7,16 @@ namespace correlation {
 
 class ZScoreEval : public BaseEval {
   public:
+    ZScoreEval();
+
     virtual void evaluateCorrelationFromTranslation(
         const std::vector<double>& corr) override;
 
   private:
     void calculateSmoothedZScore(const std::vector<double>& input, 
-        std::vector<int>* signals) const;
+        std::vector<bool>* signals) const;
     double mean(const std::vector<double>& vec) const;
-    double stdDev(const std::vector<double>& vec) const;
+    double stdDev(const double mean, const std::vector<double>& vec) const;
 };
 
 } // namespace correlation
--- src/backend/correlation/z-score-eval.cc
+++ src/backend/correlation/z-score-eval.cc
@@ -2,71 +2,82 @@
 
 #include <cmath>
 #include <cstdint>
+#include <numeric>
+#include <algorithm>
+
+#include <glog/logging.h>
+
+DEFINE_double(z_score_lag, 3500, 
+    "The window used for smoothing the function.");
+DEFINE_double(z_score_threshold, 12.5, 
+    "Defines the number of n-std requires to include a signal.");
+DEFINE_double(z_score_influence, 0.2, 
+    "The influence of the current data point to the lag mean.");
 
 namespace correlation {
 
-  void ZScoreEval::evaluateCorrelationFromTranslation(
+ZScoreEval::ZScoreEval() {
+  CHECK_GT(FLAGS_z_score_lag, 0);
+  CHECK_GT(FLAGS_z_score_threshold, 0);
+  CHECK_GE(FLAGS_z_score_influence, 0);
+  CHECK_LE(FLAGS_z_score_influence, 1.0);
+}
+
+void ZScoreEval::evaluateCorrelationFromTranslation(
       const std::vector<double>& corr) {
-    std::vector<int> signals(corr.size());
+    std::vector<bool> signals(corr.size(), false);
     calculateSmoothedZScore(corr, &signals);
 }
 
 void ZScoreEval::calculateSmoothedZScore(const std::vector<double>& input, 
-    std::vector<int>* signals) const {
- //lag 5 for the smoothing functions
-    size_t lag = 5;
-    //3.5 standard deviations for signal
-    double threshold = 3.5;
-    //between 0 and 1, where 1 is normal influence, 0.5 is half
-    double influence = .5;
+    std::vector<bool>* signals) const {
+  const uint32_t n_input = input.size();
+  if (n_input <= FLAGS_z_score_lag + 2) return;
 
-    const uint32_t n_input = input.size();
+  std::vector<double> filteredY(n_input, 0.0);
+  std::vector<double> avgFilter(n_input, 0.0);
+  std::vector<double> stdFilter(n_input, 0.0);
+  const std::vector<double> subVecStart(input.begin(), 
+      input.begin() + FLAGS_z_score_lag);
+  const double m = mean(subVecStart);
+  avgFilter[FLAGS_z_score_lag] = m;
+  stdFilter[FLAGS_z_score_lag] = stdDev(m, subVecStart);
 
-    if (input.size() <= lag + 2)
-        return;
-
-    //Initialise variables
-    std::vector<double> filteredY(input.size(), 0.0);
-    std::vector<double> avgFilter(input.size(), 0.0);
-    std::vector<double> stdFilter(input.size(), 0.0);
-    std::vector<double> subVecStart(input.begin(), input.begin() + lag);
-    avgFilter[lag] = mean(subVecStart);
-    stdFilter[lag] = stdDev(subVecStart);
-
-    for (size_t i = lag + 1; i < n_input; ++i)
-    {
-        if (std::abs(input[i] - avgFilter[i - 1]) > threshold * stdFilter[i - 1])
-        {
-            if (input[i] > avgFilter[i - 1])
-            {
-                (*signals)[i] = 1; 
-            }
-            else
-            {
-                (*signals)[i] = -1;
-            }
-            //Make influence lower
-            filteredY[i] = influence* input[i] + (1 - influence) * filteredY[i - 1];
-        }
-        else
-        {
-            (*signals)[i] = 0; //# No signal
-            filteredY[i] = input[i];
-        }
-        //Adjust the filters
-        std::vector<double> subVec(filteredY.begin() + i - lag, filteredY.begin() + i);
-        avgFilter[i] = mean(subVec);
-        stdFilter[i] = stdDev(subVec);
+  const double invInfluence = 1.0 - FLAGS_z_score_influence;
+  for (uint32_t i = FLAGS_z_score_lag + 1u; i < n_input; ++i)
+  {
+    const double currentinput = input[i];
+    if (std::abs(currentinput - avgFilter[i - 1]) 
+        > FLAGS_z_score_threshold * stdFilter[i - 1]) {
+      if (currentinput > avgFilter[i - 1]) (*signals)[i] = true; 
+      
+      // Update influence with current data point.
+      filteredY[i] = FLAGS_z_score_influence * currentinput 
+        + invInfluence * filteredY[i - 1];
     }
-    //return signals;
+    else 
+      filteredY[i] = currentinput;
+
+    // Adjust the filters.
+    std::vector<double> subVec(filteredY.begin() + i - FLAGS_z_score_lag,
+        filteredY.begin() + i);
+    const double m = mean(subVec);
+    avgFilter[i] = m;
+    stdFilter[i] = stdDev(m, subVec);
+  }
 }
 
 double ZScoreEval::mean(const std::vector<double>& vec) const {
-  return 0.0;
+  double sum = std::accumulate(std::begin(vec), std::end(vec), 0.0);
+  return sum / static_cast<double>(vec.size());
 }
 
-double ZScoreEval::stdDev(const std::vector<double>& vec) const {
-  return 0.0;
+double ZScoreEval::stdDev(const double mean, 
+    const std::vector<double>& vec) const {
+  double accum = 0.0;
+  std::for_each (std::begin(vec), std::end(vec), [&](const double val) {
+    accum += (val - mean) * (val - mean);
+  });
+  return sqrt(accum / static_cast<double>(vec.size()-1u));
 }
-
 }
