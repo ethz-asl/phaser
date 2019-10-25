99ef82bc466a2602a4fb6b50ada2e9a1bf78029c
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2019-10-25 21:03:01 +0200

    increased the translation estimation window a bit. added new gtest for all clouds in the dataset

--- test/translation-alignment/translation-alignment-test.cc
+++ test/translation-alignment/translation-alignment-test.cc
@@ -24,6 +24,8 @@ class TranslationAlignmentTest : public ::testing::Test {
   protected:
     virtual void SetUp() {
       ds_ = std::make_unique<data::DatasourcePly>();
+      ds_->setDatasetFolder("/home/berlukas/Documents/"
+          "workspace/maplab/src/packlo/test/test-data/arche/");
     }
 
     registration::BaseRegistration* initializeRegistration(bool mocked) {
@@ -33,6 +35,10 @@ class TranslationAlignmentTest : public ::testing::Test {
       else
         registrator_ = std::make_unique<registration::SphRegistration>();
       return registrator_.get();
+    }
+
+    float getRandomTranslation() {
+      return distribution_(generator_); 
     }
 
     data::DatasourcePlyPtr ds_;
@@ -46,23 +52,50 @@ TEST_F(TranslationAlignmentTest, TranslationSelfSingle) {
   registration::SphRegistrationMockTranslated* reg 
     = dynamic_cast<registration::SphRegistrationMockTranslated*>(
         initializeRegistration(true));
+  // Define a random translation.
   Eigen::Vector3d trans_xyz (12.9f, 33.1f, 21.5f);
   reg->setRandomTranslation(trans_xyz(0), trans_xyz(1), trans_xyz(2));
 
   model::RegistrationResult result;
-  ds_->setDatasetFolder("/home/berlukas/Documents/"
-      "workspace/maplab/src/packlo/test/test-data/arche/");
   ds_->subscribeToPointClouds([&] (const model::PointCloudPtr& cloud) {
     CHECK(cloud);
+    // Estimate the translation.
     result = reg->registerPointCloud(cloud, cloud);    
     EXPECT_TRUE(result.foundSolutionForTranslation());
 
-    // Convert result to xyz Euler angles and compare it.
+    // Get the result and compare it.
     EXPECT_NEAR_EIGEN(-trans_xyz, result.getTranslation(), 4.0);
     ASSERT_LE(common::MetricUtils::HausdorffDistance(cloud,
           result.getRegisteredCloud()), 5.0);
   });
   ds_->startStreaming(1);
+}
+
+TEST_F(TranslationAlignmentTest, TranslationSelfAll) {
+  CHECK(ds_);
+  registration::SphRegistrationMockTranslated* reg 
+    = dynamic_cast<registration::SphRegistrationMockTranslated*>(
+        initializeRegistration(true));
+
+  model::RegistrationResult result;
+  ds_->subscribeToPointClouds([&] (const model::PointCloudPtr& cloud) {
+    CHECK(cloud);
+    // Define a new random translation for each cloud.
+    Eigen::Vector3d trans_xyz (getRandomTranslation(), 
+        getRandomTranslation(), getRandomTranslation());
+    reg->setRandomTranslation(trans_xyz(0), trans_xyz(1), trans_xyz(2));
+
+    // Estimate the translation.
+    result = reg->registerPointCloud(cloud, cloud);    
+    EXPECT_TRUE(result.foundSolutionForTranslation());
+
+    // Get the result and compare it.
+    EXPECT_NEAR_EIGEN(-trans_xyz, result.getTranslation(), 4.0);
+    EXPECT_NEAR_EIGEN(-trans_xyz, result.getTranslation(), 4.0);
+    ASSERT_LE(common::MetricUtils::HausdorffDistance(cloud,
+          result.getRegisteredCloud()), 5.0);
+  });
+  ds_->startStreaming();
 }
 
 TEST_F(TranslationAlignmentTest, TranslationEasy) {
