d646f1c5438ba745f327c820b926f4915cba9bcc
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2019-10-25 11:40:42 +0200

    added new gtest for random self rotations using all clouds

--- test/rotation-alignment/rotation-alignment-test.cc
+++ test/rotation-alignment/rotation-alignment-test.cc
@@ -8,10 +8,18 @@
 #include <gtest/gtest.h>
 #include <cmath>
 #include <memory>
+#include <random>
+#include <chrono>
 
 namespace rotation {
 
 class RotationAlignmentTest : public ::testing::Test {
+  public: 
+    RotationAlignmentTest() 
+        : distribution_(0, 2*M_PI), 
+        generator_(std::chrono::system_clock::now()
+            .time_since_epoch().count()) {}
+
   private:
     Eigen::Vector3d fromRotation(const double r11, const double r12, const double r21,
         const double r31, const double r32) {
@@ -47,11 +55,17 @@ class RotationAlignmentTest : public ::testing::Test {
                     qw*qw + qx*qx - qy*qy - qz*qz);
     }
 
+    float getRandomAngle() {
+      return distribution_(generator_); 
+    }
+
     data::DatasourcePlyPtr ds_;
     registration::BaseRegistrationPtr registrator_;                             
+    std::default_random_engine generator_;
+    std::uniform_real_distribution<float> distribution_;
 };
 
-TEST_F(RotationAlignmentTest, RotationSelf) {
+TEST_F(RotationAlignmentTest, RotationSelfSingle) {
   CHECK(ds_);
   registration::SphRegistrationMockRotated* reg = dynamic_cast<registration::
     SphRegistrationMockRotated*>(initializeRegistration(true));
@@ -71,6 +85,32 @@ TEST_F(RotationAlignmentTest, RotationSelf) {
     EXPECT_NEAR_EIGEN(-rot_xyz_rad, xyz_rad, 1);
   });
   ds_->startStreaming(1);
+}
+
+TEST_F(RotationAlignmentTest, RotationSelfAll) {
+  CHECK(ds_);
+  registration::SphRegistrationMockRotated* reg = dynamic_cast<registration::
+    SphRegistrationMockRotated*>(initializeRegistration(true));
+
+  model::RegistrationResult result;
+  ds_->setDatasetFolder("/home/berlukas/Documents/"
+      "workspace/maplab/src/packlo/test/test-data/arche/");
+  ds_->subscribeToPointClouds([&] (const model::PointCloudPtr& cloud) {
+    CHECK(cloud);
+    // Define a new random rotation for each incoming cloud.
+    Eigen::Vector3d rot_xyz_rad (getRandomAngle(), getRandomAngle(), 
+        getRandomAngle());
+    reg->setRandomRotation(rot_xyz_rad(0), rot_xyz_rad(1), rot_xyz_rad(2));
+    
+    // Register the point clouds.
+    result = reg->registerPointCloud(cloud, cloud);    
+    EXPECT_TRUE(result.foundSolutionForRotation());
+
+    // Convert result to xyz Euler angles and compare it.
+    Eigen::Vector3d xyz_rad = convertZYZtoXYZ(result.getRotation());
+    EXPECT_NEAR_EIGEN(-rot_xyz_rad, xyz_rad, 1);
+  });
+  ds_->startStreaming();
 }
 
 TEST_F(RotationAlignmentTest, RotationEasy) {
