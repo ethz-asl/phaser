ffaf659893680150a0bbc2f41f535f8ede884503
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2019-12-03 23:48:37 +0100

    added new point type which does not work yet

--- include/packlo/common/point-types.h
+++ include/packlo/common/point-types.h
@@ -1,11 +1,12 @@
-#pragma once
+#ifndef INCLUDE_PACKLO_COMMON_POINT_TYPES_H_
+#define INCLUDE_PACKLO_COMMON_POINT_TYPES_H_
 
 #include <pcl/point_types.h>
 #include <pcl/register_point_struct.h>
 #include <pcl/common/projection_matrix.h>
 #include <Eigen/Dense>
 
-// This has to be in global namespace 
+// This has to be in global namespace
 // otherwise PCL complains.
 
 struct OusterPointType {
@@ -27,10 +28,29 @@ POINT_CLOUD_REGISTER_POINT_STRUCT(OusterPointType,
     (uint16_t, signal, intensity)
     (uint8_t, ring, ring))
 
+struct SemanticKITTIPointType {
+  PCL_ADD_POINT4D
+  float semantic;
+  float instance;
+  float reflectivity;
+  float intensity;
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+} EIGEN_ALIGN16;
+
+POINT_CLOUD_REGISTER_POINT_STRUCT(
+    SemanticKITTIPointType,
+    (float, x,
+     x)(float, y,
+        y)(float, z, z)(float, semantic, semantic)(float, instance, instance)(
+        float, reflectivity, reflectivity)(float, intensity, intensity))
+
 namespace common {
-  //using Point_t = ::OusterPointType;                                            
-  using Point_t = pcl::PointXYZI;                                                 
-  using PointCloud_t = pcl::PointCloud<Point_t>;                                  
-  using PointCloud_tPtr = pcl::PointCloud<Point_t>::Ptr; 
-  using Vector_t = Eigen::Vector3d;
+// using Point_t = ::OusterPointType;
+// using Point_t = pcl::PointXYZI;
+using Point_t = ::SemanticKITTIPointType;
+using PointCloud_t = pcl::PointCloud<Point_t>;
+using PointCloud_tPtr = pcl::PointCloud<Point_t>::Ptr;
+using Vector_t = Eigen::Vector3d;
 }
+
+#endif  // INCLUDE_PACKLO_COMMON_POINT_TYPES_H_
