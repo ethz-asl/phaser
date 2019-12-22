51d0ccece043ebaddb2903b51b8656d8e72972b2
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2019-12-22 14:35:44 +0100

    added simple gaussian class

--- phaser_common/include/packlo/distribution/gaussian.h
+++ phaser_common/include/packlo/distribution/gaussian.h
@@ -2,14 +2,27 @@
 #define PACKLO_DISTRIBUTION_GAUSSIAN_H_
 
 #include <Eigen/Dense>
+#include <utility>
 #include "packlo/distribution/base-distribution.h"
 
 namespace common {
 
 class Gaussian : public BaseDistribution {
  public:
-  explicit(Eigen::VectorXd mu, Eigen::MatrixXd C);
-}
+  explicit Gaussian(Eigen::VectorXd mu, Eigen::MatrixXd cov);
+
+  Eigen::VectorXd& getMean();
+  const Eigen::VectorXd& getMean() const;
+
+  Eigen::MatrixXd& getCov();
+  const Eigen::MatrixXd& getCov() const;
+
+  std::pair<Eigen::VectorXd, Eigen::MatrixXd> getParameters() const;
+
+ private:
+  Eigen::VectorXd mu_;
+  Eigen::MatrixXd cov_;
+};
 
 }  // namespace common
 
--- phaser_common/src/distribution/gaussian.cc
+++ phaser_common/src/distribution/gaussian.cc
@@ -1,3 +1,28 @@
 #include "packlo/distribution/gaussian.h"
 
-namespace common {}  // namespace common
+namespace common {
+
+Gaussian::Gaussian(Eigen::VectorXd mu, Eigen::MatrixXd cov)
+    : mu_(mu), cov_(cov) {}
+
+Eigen::VectorXd& Gaussian::getMean() {
+  return mu_;
+}
+
+const Eigen::VectorXd& Gaussian::getMean() const {
+  return mu_;
+}
+
+Eigen::MatrixXd& Gaussian::getCov() {
+  return cov_;
+}
+
+const Eigen::MatrixXd& Gaussian::getCov() const {
+  return cov_;
+}
+
+std::pair<Eigen::VectorXd, Eigen::MatrixXd> Gaussian::getParameters() const {
+  return std::make_pair(mu_, cov_);
+}
+
+}  // namespace common
