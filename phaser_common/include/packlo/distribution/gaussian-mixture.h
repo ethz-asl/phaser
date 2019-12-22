f7bf03d8841190999e2cb746806766084b637ec0
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2019-12-22 16:30:38 +0100

    added mixture mean and cov calculation

--- phaser_common/include/packlo/distribution/gaussian-mixture.h
+++ phaser_common/include/packlo/distribution/gaussian-mixture.h
@@ -2,6 +2,7 @@
 #define PACKLO_DISTRIBUTION_GAUSSIAN_MIXTURE_H_
 
 #include <Eigen/Dense>
+#include <utility>
 #include <vector>
 
 namespace common {
@@ -9,18 +10,23 @@ namespace common {
 class GaussianMixture {
  public:
   explicit GaussianMixture(
-      const std::vector<Eigen::VectorXd>& means,
-      const std::vector<Eigen::MatrixXd>& covs);
+      const Eigen::MatrixXd& means, const std::vector<Eigen::MatrixXd>& covs);
   explicit GaussianMixture(
-      const std::vector<Eigen::VectorXd>& means,
-      const std::vector<Eigen::MatrixXd>& covs, const Eigen::VectorXd& weights);
+      const Eigen::MatrixXd& means, const std::vector<Eigen::MatrixXd>& covs,
+      const Eigen::VectorXd& weights);
 
  private:
   void initializeUniformWeights();
+  std::pair<Eigen::VectorXd, Eigen::MatrixXd> calcMixutreParameters();
 
-  std::vector<Eigen::VectorXd> means_;
+  // Components.
+  Eigen::VectorXd means_;
+  Eigen::MatrixXd cov;
   std::vector<Eigen::MatrixXd> covs_;
   Eigen::VectorXd weights_;
+
+  Eigen::VectorXd mean_;
+  Eigen::MatrixXd cov_;
 };
 
 }  // namespace common
--- phaser_common/src/distribution/gaussian-mixture.cc
+++ phaser_common/src/distribution/gaussian-mixture.cc
@@ -1,23 +1,56 @@
 #include "packlo/distribution/gaussian-mixture.h"
 
+#include <glog/logging.h>
+
 namespace common {
 
 GaussianMixture::GaussianMixture(
-    const std::vector<Eigen::VectorXd>& means,
-    const std::vector<Eigen::MatrixXd>& covs)
+    const Eigen::MatrixXd& means, const std::vector<Eigen::MatrixXd>& covs)
     : means_(means), covs_(covs) {
   initializeUniformWeights();
+  std::tie(mean_, cov_) = calcMixutreParameters();
 }
 
 GaussianMixture::GaussianMixture(
-    const std::vector<Eigen::VectorXd>& means,
-    const std::vector<Eigen::MatrixXd>& covs, const Eigen::VectorXd& weights)
-    : means_(means), covs_(covs), weights_(weights) {}
+    const Eigen::MatrixXd& means, const std::vector<Eigen::MatrixXd>& covs,
+    const Eigen::VectorXd& weights)
+    : means_(means), covs_(covs), weights_(weights) {
+  std::tie(mean_, cov_) = calcMixutreParameters();
+}
 
 void GaussianMixture::initializeUniformWeights() {
   const double n_dim = means_.size();
   weights_ = Eigen::VectorXd::Zero(n_dim);
   weights_.array() = 1 / n_dim;
+}
+
+std::pair<Eigen::VectorXd, Eigen::MatrixXd>
+GaussianMixture::calcMixutreParameters() {
+  CHECK(means_.rows() != 0);
+  CHECK_EQ(means_.cols(), covs_.size());
+  CHECK_EQ(means_.cols(), weights_.rows());
+  // Compute sample mean and covariance.
+  Eigen::VectorXd sample_mean = means_ * weights_;
+  const std::size_t n_samples = means_.cols();
+  Eigen::MatrixXd weighted_zero_means =
+      Eigen::MatrixXd::Zero(means_.rows(), means_.cols());
+  for (std::size_t i = 0u; i < n_samples; ++i) {
+    Eigen::VectorXd zero_mean_sample = means_.col(i) - sample_mean;
+    weighted_zero_means.col(i) = zero_mean_sample * std::sqrt(weights_(i));
+  }
+  Eigen::MatrixXd cov_mean =
+      weighted_zero_means * weighted_zero_means.transpose();
+
+  // Combine the covariances with the sample covariance.
+  Eigen::MatrixXd sum_cov =
+      Eigen::MatrixXd::Zero(cov_mean.rows(), cov_mean.cols());
+  for (std::size_t i = 0u; i < n_samples; ++i) {
+    sum_cov += covs_[i] * weights_(i);
+  }
+  Eigen::MatrixXd cov = sum_cov + cov_mean;
+
+  return std::make_pair<Eigen::VectorXd, Eigen::MatrixXd>(
+      std::move(sample_mean), std::move(cov));
 }
 
 }  // namespace common
