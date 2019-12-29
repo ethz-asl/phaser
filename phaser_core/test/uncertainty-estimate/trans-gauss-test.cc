39664426dc8c60c1c2471372acc72fca82e2db80
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2019-12-29 13:40:39 +0100

    added simple low uncertainty test

---
+++ phaser_core/test/uncertainty-estimate/trans-gauss-test.cc
@@ -0,0 +1,48 @@
+#include "packlo/backend/registration/sph-registration.h"
+#include "packlo/common/data/datasource-ply.h"
+#include "packlo/common/metric-utils.h"
+#include "packlo/common/test/testing-entrypoint.h"
+#include "packlo/common/test/testing-predicates.h"
+
+#include <gtest/gtest.h>
+
+namespace uncertainty {
+
+class TransGaussTest : public ::testing::Test {
+ protected:
+  virtual void SetUp() {
+    ds_ = std::make_unique<data::DatasourcePly>();
+    CHECK_NOTNULL(ds_);
+    ds_->setDatasetFolder("./test_clouds/arche/");
+    registrator_ = std::make_unique<registration::SphRegistration>();
+  }
+
+  data::DatasourcePlyPtr ds_;
+  registration::BaseRegistrationPtr registrator_;
+};
+
+TEST_F(TransGaussTest, LowUncertainty) {
+  CHECK(ds_);
+
+  model::RegistrationResult result;
+  model::PointCloudPtr prev_cloud = nullptr;
+  ds_->subscribeToPointClouds([&](const model::PointCloudPtr& cloud) {
+    CHECK(cloud);
+    if (prev_cloud == nullptr) {
+      prev_cloud = cloud;
+      prev_cloud->initialize_kd_tree();
+      return;
+    }
+
+    // Register the point clouds.
+    cloud->initialize_kd_tree();
+    result = registrator_->registerPointCloud(prev_cloud, cloud);
+    prev_cloud = cloud;
+    EXPECT_TRUE(result.foundSolutionForRotation());
+  });
+  ds_->startStreaming();
+}
+
+}  // namespace uncertainty
+
+MAPLAB_UNITTEST_ENTRYPOINT
