6602e9f3b749bba4d32ea263d80c908521f41d64
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2020-01-07 10:03:28 +0100

    added bingham ctor test

--- phaser_common/test/distribution/bingham-test.cc
+++ phaser_common/test/distribution/bingham-test.cc
@@ -21,8 +21,16 @@ class BinghamTest : public ::testing::Test {
 };
 
 TEST_F(BinghamTest, constructorTest) {
-  // EXPECT_NEAR_EIGEN(gauss.getMean(), true_mean, 1e-4);
-  // EXPECT_NEAR_EIGEN(gauss.getCov(), true_cov, 1e-4);
+  Eigen::MatrixXd M = Eigen::MatrixXd::Identity(2, 2);
+  Eigen::Vector2d Z(-3, 0);
+  common::Bingham bingham(Z, M);
+
+  Eigen::Vector2d true_mode(0, 1);
+  Eigen::MatrixXd true_moment(2, 2);
+  true_moment << 0.201933380584355, 0, 0, 0.798066619415645;
+
+  EXPECT_NEAR_EIGEN(bingham.mode(), true_mode, 1e-4);
+  EXPECT_NEAR_EIGEN(bingham.moment(), true_moment, 1e-4);
 }
 
 }  // namespace common
