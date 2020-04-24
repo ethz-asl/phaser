949092f1f26b5e42be841126bb9186dbde71412f
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2020-04-24 10:45:53 +0200

    refactored seq corr version

--- phaser_core/src/backend/alignment/spatial-correlation.cc
+++ phaser_core/src/backend/alignment/spatial-correlation.cc
@@ -7,7 +7,6 @@ namespace alignment {
 SpatialCorrelation::SpatialCorrelation(const uint32_t n_voxels)
     : total_n_voxels_(n_voxels * n_voxels * n_voxels),
       n_voxels_per_dim_(n_voxels) {
-
   const uint32_t n_fftw_size = sizeof(fftw_complex) * total_n_voxels_;
   F_ = static_cast<fftw_complex*>(fftw_malloc(n_fftw_size));
   G_ = static_cast<fftw_complex*>(fftw_malloc(n_fftw_size));
@@ -18,14 +17,14 @@ SpatialCorrelation::SpatialCorrelation(const uint32_t n_voxels)
   g_ = new double[total_n_voxels_];
 
   // Create the FFTW plans for two FFTs and one IFFT.
-  f_plan_ = fftw_plan_dft_r2c_3d(n_voxels, n_voxels, n_voxels,
-    f_, F_, FFTW_ESTIMATE);
+  f_plan_ =
+      fftw_plan_dft_r2c_3d(n_voxels, n_voxels, n_voxels, f_, F_, FFTW_ESTIMATE);
 
-  g_plan_ = fftw_plan_dft_r2c_3d(n_voxels, n_voxels, n_voxels,
-    g_, G_, FFTW_ESTIMATE);
+  g_plan_ =
+      fftw_plan_dft_r2c_3d(n_voxels, n_voxels, n_voxels, g_, G_, FFTW_ESTIMATE);
 
-  c_plan_ = fftw_plan_dft_c2r_3d(n_voxels, n_voxels, n_voxels,
-    C_, c_, FFTW_ESTIMATE);
+  c_plan_ =
+      fftw_plan_dft_c2r_3d(n_voxels, n_voxels, n_voxels, C_, c_, FFTW_ESTIMATE);
 }
 
 SpatialCorrelation::~SpatialCorrelation() {
@@ -38,9 +37,17 @@ SpatialCorrelation::~SpatialCorrelation() {
   fftw_destroy_plan(c_plan_);
 
   fftw_cleanup();
-  delete [] c_;
-  delete [] f_;
-  delete [] g_;
+  delete[] c_;
+  delete[] f_;
+  delete[] g_;
+}
+
+void SpatialCorrelation::complexMulSeq(
+    fftw_complex* F, fftw_complex* G, fftw_complex* C) {
+  for (uint32_t i = 0u; i < total_n_voxels_; ++i) {
+    C[i][0] = F[i][0] * G[i][0] - F[i][1] * (-G[i][1]);
+    C[i][1] = F[i][0] * (-G[i][1]) + F[i][1] * G[i][0];
+  }
 }
 
 double* SpatialCorrelation::correlateSignals(double* const f, double* const g) {
@@ -55,10 +62,7 @@ double* SpatialCorrelation::correlateSignals(double* const f, double* const g) {
   fftw_execute(g_plan_);
 
   // Correlate the signals in the frequency domain.
-  for (uint32_t i = 0u; i < total_n_voxels_; ++i) {
-    C_[i][0] = F_[i][0] * G_[i][0] - F_[i][1] * (-G_[i][1]);
-    C_[i][1] = F_[i][0] * (-G_[i][1]) + F_[i][1] * G_[i][0];
-  }
+  complexMulSeq(F_, G_, C_);
 
   // Perform the IFFT on the correlation tensor.
   VLOG(1) << "Performing IFFT on correlation.";
