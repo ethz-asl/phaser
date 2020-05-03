9c91598031e5e130e51c8f7c6e624cba44daa5c2
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2020-05-03 11:45:34 +0200

    wip on the corr worker

--- phaser_core/include/phaser/backend/correlation/spherical-correlation-worker.h
+++ phaser_core/include/phaser/backend/correlation/spherical-correlation-worker.h
@@ -1,21 +1,28 @@
 #ifndef PHASER_BACKEND_CORRELATION_SPHERICAL_CORRELATION_WORKER_H_
 #define PHASER_BACKEND_CORRELATION_SPHERICAL_CORRELATION_WORKER_H_
 
+#include "phaser/backend/correlation/spherical-correlation.h"
 #include "phaser/common/base-worker.h"
+#include "phaser/common/spherical-sampler.h"
 #include "phaser/model/point-cloud.h"
 
 namespace correlation {
 
-class SphericalCorrelationWorker : public BaseWorker {
+class SphericalCorrelationWorker : public common::BaseWorker {
  public:
   explicit SphericalCorrelationWorker(
-      const model::PointCloud& source, const model::PointCloud& target);
+      model::PointCloudPtr target, model::PointCloudPtr source,
+      const uint16_t bandwidth);
 
   void run() override;
 
-  void isFinished() const override;
+  std::vector<double> getCorrelation() const noexcept;
 
  private:
+  model::PointCloudPtr source_;
+  model::PointCloudPtr target_;
+  common::SphericalSampler sampler_;
+  SphericalCorrelation sph_corr_;
 };
 
 }  // namespace correlation
---
+++ phaser_core/src/backend/correlation/spherical-correlation-worker.cc
@@ -0,0 +1,35 @@
+#include "phaser/backend/correlation/spherical-correlation-worker.h"
+
+#include <glog/logging.h>
+
+namespace correlation {
+
+SphericalCorrelationWorker::SphericalCorrelationWorker(
+    model::PointCloudPtr target, model::PointCloudPtr source,
+    const uint16_t bandwidth)
+    : source_(source), target_(target), sampler_(bandwidth) {}
+
+void SphericalCorrelationWorker::run() {
+  VLOG(1) << "[SphRegistrationWorker] Estimating rotation...";
+  source_->initialize_kd_tree();
+  target_->initialize_kd_tree();
+
+  // Sample the sphere at the grid points.
+  std::vector<model::FunctionValue> f_values;
+  std::vector<model::FunctionValue> h_values;
+  sampler_.sampleUniformly(*target_, &f_values);
+  sampler_.sampleUniformly(*source_, &h_values);
+
+  std::array<double, 3> zyz;
+  sph_corr_.correlateSignals(
+      f_values, h_values, sampler_.getInitializedBandwith(), &zyz);
+
+  is_completed_ = true;
+}
+
+std::vector<double> SphericalCorrelationWorker::getCorrelation() const
+    noexcept {
+  return sph_corr_.getCorrelation();
+}
+
+}  // namespace correlation
