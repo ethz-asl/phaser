80783b5c9df9d0105bc0db615f07f9f7805c6818
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2020-05-10 16:33:12 +0200

    added fftshift

---
+++ phaser_common/include/phaser/common/signal-utils.h
@@ -0,0 +1,26 @@
+#ifndef PHASER_COMMON_SIGNAL_UTILS_H_
+#define PHASER_COMMON_SIGNAL_UTILS_H_
+
+#include <glog/logging.h>
+
+namespace common {
+
+class SignalUtils {
+ public:
+  template <typename T_input>
+  static void FFTShift(T_input* input, const std::size_t n);
+};
+
+template <typename T_input>
+void SignalUtils::FFTShift(T_input* input, const std::size_t n) {
+  CHECK_NOTNULL(input);
+  if (n % 2 == 0) {
+    std::rotate(&input[0], &input[n >> 1], &input[n]);
+  } else {
+    std::rotate(&input[0], &input[(n >> 1) + 1], &input[n]);
+  }
+}
+
+}  // namespace common
+
+#endif  // PHASER_COMMON_SIGNAL_UTILS_H_
---
+++ phaser_common/test/utils/signal-utils-test.cc
@@ -0,0 +1,74 @@
+#include <chrono>
+#include <cmath>
+#include <memory>
+#include <random>
+
+#include <Eigen/Dense>
+#include <glog/logging.h>
+#include <gtest/gtest.h>
+
+#include "phaser/common/signal-utils.h"
+#include "phaser/common/test/testing-entrypoint.h"
+#include "phaser/common/test/testing-predicates.h"
+
+namespace common {
+
+class SignalUtilsTest : public ::testing::Test {
+ public:
+  SignalUtilsTest()
+      : generator_(std::chrono::system_clock::now().time_since_epoch().count()),
+        distribution_(0, 100) {}
+
+  float getRandomFloat() {
+    return distribution_(generator_);
+  }
+
+ private:
+  std::default_random_engine generator_;
+  std::uniform_real_distribution<float> distribution_;
+};
+
+TEST_F(SignalUtilsTest, FFTShiftDoubleEvenTest) {
+  const std::size_t n_points = 10;
+  const std::size_t n_points_half = n_points / 2;
+  std::vector<double> shifted(n_points);
+  std::vector<double> original(n_points);
+  for (std::size_t i = 0; i < n_points; ++i) {
+    const float val = getRandomFloat();
+    shifted[i] = val;
+    original[i] = val;
+  }
+  SignalUtils::FFTShift(shifted.data(), n_points);
+
+  constexpr float tol = 0.001;
+  for (std::size_t i = 0; i < n_points_half; ++i) {
+    EXPECT_NEAR(original[i], shifted[i + n_points_half], tol);
+    EXPECT_NEAR(original[i + n_points_half], shifted[i], tol);
+  }
+}
+
+TEST_F(SignalUtilsTest, FFTShiftDoubleOddTest) {
+  const std::size_t n_points = 7;
+  const std::size_t n_points_half = floor(n_points / 2.0);
+  std::vector<double> shifted(n_points);
+  std::vector<double> original(n_points);
+  for (std::size_t i = 0; i < n_points; ++i) {
+    const float val = getRandomFloat();
+    shifted[i] = val;
+    original[i] = val;
+  }
+  SignalUtils::FFTShift(shifted.data(), n_points);
+
+  constexpr float tol = 0.001;
+  const std::size_t n_points_remaining = n_points - n_points_half;
+  for (std::size_t i = 0; i < n_points_remaining; ++i) {
+    EXPECT_NEAR(original[i], shifted[i + n_points_half], tol);
+  }
+  for (std::size_t i = 0; i < n_points_half; ++i) {
+    EXPECT_NEAR(original[i + n_points_half + 1], shifted[i], tol);
+  }
+}
+
+}  // namespace common
+
+MAPLAB_UNITTEST_ENTRYPOINT
