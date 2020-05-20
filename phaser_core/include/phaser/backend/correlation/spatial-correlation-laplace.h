f4cd19f250a7e716e0aa61d65770663518cbd6b8
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2020-05-20 13:30:01 +0200

    added initial skeleton

---
+++ phaser_core/include/phaser/backend/correlation/spatial-correlation-laplace.h
@@ -0,0 +1,20 @@
+#ifndef PHASER_BACKEND_CORRELATION_SPATIAL_CORRELATION_LAPLACE_H_
+#define PHASER_BACKEND_CORRELATION_SPATIAL_CORRELATION_LAPLACE_H_
+
+#include <vector>
+
+#include "phaser/backend/correlation/spatial-correlation.h"
+
+namespace correlation {
+
+class SpatialCorrelationLaplace : public SpatialCorrelation {
+ public:
+  explicit SpatialCorrelationLaplace(const uint32_t n_voxels);
+
+  virtual ~SpatialCorrelationLaplace() = default;
+  double* correlateSignals(double* const f, double* const g) override;
+};
+
+}  // namespace correlation
+
+#endif  // PHASER_BACKEND_CORRELATION_SPATIAL_CORRELATION_LAPLACE_H_
---
+++ phaser_core/src/backend/correlation/spatial-correlation-laplace.cc
@@ -0,0 +1,37 @@
+#include "phaser/backend/correlation/spatial-correlation-laplace.h"
+
+#include <glog/logging.h>
+
+#include "phaser/common/signal-utils.h"
+
+namespace correlation {
+
+SpatialCorrelationLaplace::SpatialCorrelationLaplace(const uint32_t n_voxels)
+    : SpatialCorrelation(n_voxels) {}
+
+double* SpatialCorrelationLaplace::correlateSignals(
+    double* const f, double* const g) {
+  const uint32_t function_size = total_n_voxels_ * sizeof(double);
+  memcpy(f_, f, function_size);
+  memcpy(g_, g, function_size);
+
+  // Perform the two FFTs on the discretized signals.
+  VLOG(1) << "Performing FFT on the first point cloud.";
+  fftw_execute(f_plan_);
+  VLOG(1) << "Performing FFT on the second point cloud.";
+  fftw_execute(g_plan_);
+
+  common::SignalUtils::FFTShift(F_, total_n_voxels_);
+  common::SignalUtils::FFTShift(G_, total_n_voxels_);
+
+  complexMulSeq(F_, G_, C_);
+
+  // Perform the IFFT on the correlation tensor.
+  VLOG(1) << "Shifting back the signals. Performing IFFT on low passed filtered"
+             " correlation.";
+  common::SignalUtils::IFFTShift(C_, total_n_voxels_);
+  fftw_execute(c_plan_);
+  return c_;
+}
+
+}  // namespace correlation
