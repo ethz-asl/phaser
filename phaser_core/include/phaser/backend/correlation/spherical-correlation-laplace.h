7d49b7e38c7b0d861b946f0755a8ebe22c5f3498
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2020-05-23 16:34:08 +0200

    sph laplace is ready for testing

--- phaser_core/include/phaser/backend/correlation/spherical-correlation-laplace.h
+++ phaser_core/include/phaser/backend/correlation/spherical-correlation-laplace.h
@@ -13,10 +13,22 @@ class SphericalCorrelationLaplace : public SphericalCorrelation {
   explicit SphericalCorrelationLaplace(const uint32_t bw = 100);
   virtual ~SphericalCorrelationLaplace() = default;
   void correlateSampledSignals(
-      const std::vector<SampledSignal>& f1,
-      const std::vector<SampledSignal>& f2) override;
+      const std::vector<SampledSignal>& f,
+      const std::vector<SampledSignal>& g) override;
 
  protected:
+  std::pair<fftw_complex*, fftw_complex*> performFFTandShift(
+      const SampledSignal& f1, const SampledSignal& f2,
+      const uint32_t n_coeffs);
+  fftw_complex* convertCoeffArrays(
+      double* f_real, double* f_imag, const uint32_t n_coeffs);
+  void setFusedCoefficients(
+      const std::vector<fusion::complex_t>& signal,
+      const std::vector<fusion::complex_t>& pattern, const uint32_t n_coeffs);
+
+  void shiftSignals(const uint32_t n_points);
+  void inverseShiftSignals(const uint32_t n_points);
+
   fusion::LaplacePyramid laplace_;
 };
 
--- phaser_core/src/backend/correlation/spherical-correlation-laplace.cc
+++ phaser_core/src/backend/correlation/spherical-correlation-laplace.cc
@@ -10,14 +10,83 @@ SphericalCorrelationLaplace::SphericalCorrelationLaplace(const uint32_t bw)
     : SphericalCorrelation(bw) {}
 
 void SphericalCorrelationLaplace::correlateSampledSignals(
-    const std::vector<SampledSignal>& f1,
-    const std::vector<SampledSignal>& f2) {
-  CHECK_EQ(f1.size(), f2.size());
-  CHECK_GT(f1.size(), 0u);
+    const std::vector<SampledSignal>& f, const std::vector<SampledSignal>& g) {
+  CHECK_EQ(f.size(), g.size());
+  CHECK_GT(f.size(), 0u);
   VLOG(1) << "--- Spherical laplace correlation [" << bw_ << " bw] -----";
-  performSphericalTransforms(f1[0], f2[0]);
+  const uint32_t full_bw = bw_ * bw_;
+  VLOG(2) << "Performing SFT (1/2)";
+  auto F1G1 = performFFTandShift(f[0], g[0], full_bw);
+  VLOG(2) << "Performing SFT (2/2)";
+  auto F2G2 = performFFTandShift(f[1], g[1], full_bw);
+
+  VLOG(2) << "Fusing channels together.";
+  std::vector<fftw_complex*> channels = {F1G1.first, F2G2.first};
+  std::vector<fusion::complex_t> F_fused =
+      laplace_.fuseChannels(channels, full_bw, 3);
+  channels = {F1G1.second, F2G2.second};
+  std::vector<fusion::complex_t> G_fused =
+      laplace_.fuseChannels(channels, full_bw, 3);
+
+  VLOG(2) << "Setting the fused values for the original input.";
+  setFusedCoefficients(F_fused, G_fused, full_bw);
+  VLOG(2) << "Shifting back the signals.";
+  inverseShiftSignals(full_bw);
+
+  VLOG(2) << "Correlating and inverse transform.";
   correlate();
   inverseTransform();
+
+  VLOG(2) << "Deleting the created values.";
+  delete[] F1G1.first;
+  delete[] F1G1.second;
+  delete[] F2G2.first;
+  delete[] F2G2.second;
+}
+
+std::pair<fftw_complex*, fftw_complex*>
+SphericalCorrelationLaplace::performFFTandShift(
+    const SampledSignal& f1, const SampledSignal& f2, const uint32_t n_coeffs) {
+  performSphericalTransforms(f1, f2);
+  shiftSignals(n_coeffs);
+  return std::make_pair(
+      convertCoeffArrays(sig_coef_[0], sig_coef_[1], n_coeffs),
+      convertCoeffArrays(pat_coef_[0], pat_coef_[1], n_coeffs));
+}
+
+fftw_complex* SphericalCorrelationLaplace::convertCoeffArrays(
+    double* f_real, double* f_imag, const uint32_t n_coeffs) {
+  fftw_complex* coeffs = new fftw_complex[n_coeffs];
+  for (uint32_t i = 0; i < n_coeffs; ++i) {
+    coeffs[i][0] = f_real[i];
+    coeffs[i][1] = f_imag[i];
+  }
+  return coeffs;
+}
+
+void SphericalCorrelationLaplace::setFusedCoefficients(
+    const std::vector<fusion::complex_t>& signal,
+    const std::vector<fusion::complex_t>& pattern, const uint32_t n_coeffs) {
+  for (uint32_t i = 0; i < n_coeffs; ++i) {
+    sig_coef_[0][i] = signal[i][0];
+    sig_coef_[1][i] = signal[i][1];
+    pat_coef_[0][i] = pattern[i][0];
+    pat_coef_[1][i] = pattern[i][1];
+  }
+}
+
+void SphericalCorrelationLaplace::shiftSignals(const uint32_t n_points) {
+  common::SignalUtils::FFTShift(sig_coef_[0], n_points);
+  common::SignalUtils::FFTShift(sig_coef_[1], n_points);
+  common::SignalUtils::FFTShift(pat_coef_[0], n_points);
+  common::SignalUtils::FFTShift(pat_coef_[1], n_points);
+}
+
+void SphericalCorrelationLaplace::inverseShiftSignals(const uint32_t n_points) {
+  common::SignalUtils::IFFTShift(sig_coef_[0], n_points);
+  common::SignalUtils::IFFTShift(sig_coef_[1], n_points);
+  common::SignalUtils::IFFTShift(pat_coef_[0], n_points);
+  common::SignalUtils::IFFTShift(pat_coef_[1], n_points);
 }
 
 }  // namespace correlation
