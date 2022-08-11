ba385f058f25c085a60f8ec5a5f417070e437116
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2022-08-11 11:53:45 +0200

    added missing parts

--- phaser_deploy/install_packages.sh
+++ phaser_deploy/install_packages.sh
@@ -2,7 +2,7 @@
 
 apt update
 apt -y install clang-format-6.0 ros-melodic-pcl-conversions \
-  libpcl-dev libnlopt-dev ros-melodic-pcl-ros wget
+  libpcl-dev libnlopt-dev ros-melodic-pcl-ros wget zsh libfftw3-dev
 
 wstool init
 wstool merge phaser/dependencies.rosinstall
