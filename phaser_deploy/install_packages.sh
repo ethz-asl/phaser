639a7da4a4803853f8b90783402431d4278adf8d
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2022-08-11 10:58:49 +0200

    fixing some scripts

--- phaser_deploy/install_packages.sh
+++ phaser_deploy/install_packages.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env zsh
 
+apt update
 apt -y install clang-format-6.0 ros-melodic-pcl-conversions \
   libpcl-dev libnlopt-dev ros-melodic-pcl-ros wget
 
-  wstool init
-  wstool merge phaser/dependencies.rosinstall
-  wstool update
+wstool init
+wstool merge phaser/dependencies.rosinstall
+wstool update
